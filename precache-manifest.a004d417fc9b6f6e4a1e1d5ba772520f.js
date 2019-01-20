d90d78ea0eb07fe7d96126351c78480c6a172e1a
callumgrayson <ztlimp@gmail.com>
2019-01-20 20:37:27 +0800

    Updates

---
+++ precache-manifest.a004d417fc9b6f6e4a1e1d5ba772520f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "53ccb5121c849fce0dc6",
+    "url": "/jammming/static/js/runtime~main.53ccb512.js"
+  },
+  {
+    "revision": "86a28d440825f96a8a12",
+    "url": "/jammming/static/js/main.86a28d44.chunk.js"
+  },
+  {
+    "revision": "0a29ead9173a99ed3b8a",
+    "url": "/jammming/static/js/1.0a29ead9.chunk.js"
+  },
+  {
+    "revision": "86a28d440825f96a8a12",
+    "url": "/jammming/static/css/main.244d4183.chunk.css"
+  },
+  {
+    "revision": "e1e2add83d09c6a96dd3dc8f46789e07",
+    "url": "/jammming/index.html"
+  }
+];
